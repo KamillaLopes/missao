ee84da1b38c7e8a7f6100f5cc0c6029580ba856d
Kamilla Lopes <105080698+KamillaLopes@users.noreply.github.com>
2024-06-04 11:51:05 -0300

    ok

---
+++ script.js
@@ -0,0 +1,7 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPergunta = document.querySelector(".caixa-pergunta");
+const caixaAlternativa = document.querySelector(".caixa-alternativa");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado - document.querySelector(".texto-resultado");
+
+const perguntas = ["pergunta1", "pergunta2"];
