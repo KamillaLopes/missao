4947a385d79947905214e1863d44e8c1ae1451c0
Kamilla Lopes <105080698+KamillaLopes@users.noreply.github.com>
2024-06-11 12:05:40 -0300

    ok

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const caixaPrincipal = document.querySelector(".caixa-principal");
-const caixaPergunta = document.querySelector(".caixa-pergunta");
+const caixaPerguntas = document.querySelector(".caixa-pergunta");
 const caixaAlternativa = document.querySelector(".caixa-alternativa");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
@@ -11,10 +11,20 @@ const perguntas = [
                         "implementação de leis mais rigorosas para a proteção das florestas"]
     },
 
+
     { //segundo objeto da lista de perguntas
-        enunciado: "como podemos compbater a desigualdade social de forma mias eficaz e sustentável?", 
+        enunciado: "como podemos combater a desigualdade social de forma mais eficaz e sustentável?", 
         alternativas: ["implementar programas de educação e formação profissional acessíveis a todos",
                         "políticas de redistribuição de renda e oportunidades"]
-    }
+    },
 
 ];
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta(){ //função para mostrar a pergunta
+    perguntaAtual = perguntas[atual]; //guardando a lista de perguntas dentro da variável perguntaAtual
+    caixaPerguntas.textContent = perguntaAtual.enunciado; //manipulação 
+}
+mostraPergunta();
