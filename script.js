1d67b69b9628d5ee768a26246ff29b2d4602fa3e
Kamilla Lopes <105080698+KamillaLopes@users.noreply.github.com>
2024-06-11 11:43:57 -0300

    ok

--- script.js
+++ script.js
@@ -6,13 +6,15 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     { //primeiro objeto da lista de perguntas
-        enunciado: "pergunta1", 
-        alternativas: ["alternativa1", "alternativa2"]
+        enunciado: "qual a melhor maneira de se combater o desmatamento?", 
+        alternativas: ["reflorestamento de áreas degradadas",
+                        "implementação de leis mais rigorosas para a proteção das florestas"]
     },
 
     { //segundo objeto da lista de perguntas
-        enunciado: "pergunta2", 
-        alternativas: ["alternativa1", "alternativa2"]
+        enunciado: "como podemos compbater a desigualdade social de forma mias eficaz e sustentável?", 
+        alternativas: ["implementar programas de educação e formação profissional acessíveis a todos",
+                        "políticas de redistribuição de renda e oportunidades"]
     }
 
 ];
